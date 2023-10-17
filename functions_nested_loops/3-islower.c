9db7b666fef3971758aad3ab0e03906874d27c4b
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-17 01:08:16 -0700

    3-islover.c file was changed

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * 3-islower.c - function that checks for lowercase character.
+ * _islower - function that checks for lowercase character.
  * @c: The character to be checked.
  * Return: 1 if character is lowercase, 0 otherwise.
  */
