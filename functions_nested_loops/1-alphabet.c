0521cbcc4793b05e1fe55746d33f20ebc02e8fd4
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-17 00:27:34 -0700

    1-alpahebt.c file was created

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - make the alphabet
+ * return - 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (; c < 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
