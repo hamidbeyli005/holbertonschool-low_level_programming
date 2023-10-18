2ea71dbcf60dbf9662b81b8b0c5563140bcf58b1
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-18 07:41:36 -0700

    11-print_to_98.c filw was changed

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - function that prints all natural numbers
- * from n to 98, followed by a new line.
- * @n - number.
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: The number to start printing from
+  *
+  * Return: Always 0.
  */
 
 void print_to_98(int n)
