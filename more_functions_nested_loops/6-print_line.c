d469d04bfb6598caa0954991c9d63f82ebe4397d
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-21 05:51:12 -0700

    6-print_line.c file was created

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ * @n: is the integer for the paramaters of my function
+*/
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+	 _putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
