1493d7f519a31d9596a3573beea1f3c206a5401e
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-24 00:22:23 -0700

    4-print_rev.c file was created

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string,
+ * in reverse, followed by a new line.
+ * @s:  char
+ *
+ * Return: always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		_putchar (s[i]);
+		i--;
+	}
+	_putchar ('\n');
+}
