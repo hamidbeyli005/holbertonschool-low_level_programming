7e0fe1426bc43068f0db9edeea9cbafefc3305ca
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-23 23:33:06 -0700

    Initial commit

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter 
- * and updates the value it points to to 98. 
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
  * @n: pointer to n
  * Return: Always 0.
  */
