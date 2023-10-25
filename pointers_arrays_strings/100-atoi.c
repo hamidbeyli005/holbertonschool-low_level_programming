df1ed4a406bbf26c22eb300580fa635922280715
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-25 02:38:44 -0700

    100-atoic.c file was created

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,40 +6,28 @@
  *
  * Return: Converted integer. If no valid integer is found, return 0.
  */
+
 int _atoi(char *s)
 {
 	int sign = 1;
 	int result = 0;
 	int digit;
+	short found_digit = 0;
 
 	while (*s)
 	{
 		if (*s == '-')
 			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
+		if (*s >= '0' && *s <= '9')
 		{
 			digit = *s - '0';
-
-			if (result > INT_MAX / 10)
-			{
-				if (sign == 1)
-					return (INT_MAX);
-				else
-					return (INT_MIN);
-			}
-			if (result * 10 > INT_MAX - digit)
-			{
-				if (sign == 1)
-					return (INT_MAX);
-				else
-					return (INT_MIN);
-			}
-
-			result = result * 10 + digit;
+			result = result * 10 - digit;
+			found_digit = 1;
 		}
-		else if (result > 0)
+		else if (found_digit)
+		{
 			break;
-
+		}
 		s++;
 	}
 
