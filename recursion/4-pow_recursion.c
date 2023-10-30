165d7de0e045fab15c3fc312af701bce5dec8b0d
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-30 01:41:31 -0700

    4-pow_recursion.c file was changed

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
- * _pow_recursion - return function that returns
- * the value of x raised to the power of y.
- * @x: value of raised.
- * @y: power.
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
  *
- * Return : result of the power.
-*/
+ * Return: result of the power
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
