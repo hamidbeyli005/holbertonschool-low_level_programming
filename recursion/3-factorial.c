852f1ddfbe9f7b345c501e5e4c1c667197ed49f6
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-10-30 01:40:07 -0700

    3-factorial.c file was changed

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * factorial - returns the factorial of a number
- * @n: number.
+ * @n: number to return the factorial from
  *
- * Return : factorial n.
-*/
+ * Return: factorial of n
+ */
 int factorial(int n)
 {
 	if (n == 1 || n == 0)
