410a98a63de524533cd834bc86459783202fdca6
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-11-04 11:54:50 -0700

    first commit

---
+++ static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strchr -  locates a character in a string
+ *
+ * @s: the string to check
+ * @c: the character we're looking for
+ *
+ * Return: a pointer to the first occurance of the character @c in the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
---
+++ static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer.
+ * @c: The character to be checked.
+ * Return: The absolute value of the integer.
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (-c);
+	}
+}
