1cb23ad79687be9691298bc87918990e226415d6
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-11-10 12:27:19 -0800

    2-str_concat.c file was changed

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,11 +18,9 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 
 	if (s2 == NULL)
-		s2 = "";
-	
+	s2 = "";
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
-
 	concat = malloc(sizeof(char) * len);
 	if (concat == NULL)
 		return (NULL);
