3e87c743a713a87db8cbacb5ad65854c76d8aead
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-11-17 00:42:35 -0800

    task 3

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifdef ABS_H
+#ifndef ABS_H
 #define ABS_H
 #define ABS(x) ((x) > 0 ? (x) : (x) * -1)
 #endif
--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,4 +1,6 @@
-#ifdef SUM_H
+#ifndef SUM_H
 #define SUM_H
+
 #define SUM(x, y) ((x) + (y))
+
 #endif
