ea863efbaa8071338b5971f77c0535008947bf86
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-11-23 23:06:14 -0800

    Added dog.h file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
