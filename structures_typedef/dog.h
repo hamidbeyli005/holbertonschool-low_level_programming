1f20cc31df2509c74afe6efd40fb34419c79bb5e
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-11-23 23:15:47 -0800

    Betty style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - dog data
  * @name: name of the dog
@@ -6,8 +8,10 @@
  *
  * Description: Defines metadata about the dog and type of dog's struct
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+#endif
