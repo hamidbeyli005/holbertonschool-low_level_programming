14dbc9e372f5b14571034a52c56b6ea64960aec3
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-12-03 23:46:50 -0800

    Added \n

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)");
+			printf("[0] (nill)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		nodes++;
