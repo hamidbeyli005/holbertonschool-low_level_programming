f741bbd44d5e7ed318a60ec52667e6b098880fa5
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-12-08 00:09:51 -0800

    Added Free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to the start of the list.
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
