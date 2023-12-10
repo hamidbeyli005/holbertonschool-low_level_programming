ff396f01805f0c90f3ad82cc75b74c69527a1cf9
hamidbeyli005 <hemidbeyli2020@gmail.com>
2023-12-10 02:36:40 -0800

    Kernel style code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,30 +16,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	
 	for (i = 0; i < idx - 1 && current != NULL; i++)
 		current = current->next;
-	
+
 	if (current == NULL && i == idx)
 		return (add_dnodeint_end(h, n));
 		else if (current == NULL)
 		return (NULL);
 		new_node = malloc(sizeof(dlistint_t));
-		
+
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = current->next;
 	new_node->prev = current;
-	
+
 	if (current->next != NULL)
 		current->next->prev = new_node;
-	
+
 	current->next = new_node;
-	
+
 	return (new_node);
 }
