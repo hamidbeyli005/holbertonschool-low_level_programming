c4578fcdd71aaaac75024fffbdfd790d6ae62779
Hamid <hemidbeyli2020@gmail.com>
2024-01-26 04:15:43 -0800

    Added jump

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,6 +6,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
+int print(int *array, size_t p_before, size_t step, int value);
 
 #endif
