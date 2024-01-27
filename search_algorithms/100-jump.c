8dae54ce0615141f725c7ca052f321196c86d8e0
Hamid <hemidbeyli2020@gmail.com>
2024-01-27 01:27:58 -0800

    Jump updated

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -1,66 +1,35 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searching algorithm
- * @array: array of integers, sorted
- * @size: size of the array
- * @value: key value to match an element of the array
+ * jump_search - performs jump search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
  *
- * Description: Using a jump search algorithm to find matching value
- * Return: -1 if not found or index to the matched value
+ * Return: the index found or -1
  */
 int jump_search(int *array, size_t size, int value)
 {
-	unsigned int step = sqrt(size);
-	unsigned int left = 0;
-	unsigned int right = 0;
-	unsigned int i;
+	size_t i = 0, j = sqrt(size);
 
-	if (array == NULL || size == 0)
+	if (!array)
 		return (-1);
 
-	while (left < size && array[left] <= value)
+	while (i < size && array[i] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", left, array[left]);
-		right = min(size - 1, left + step);
-		if (array[left] <= value && array[right] >= value)
-			break;
-		left += step;
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i += j;
 	}
-	if (value > array[right])
+
+	j = i - j;
+	printf("Value found between indexes [%lu] and [%lu]\n", j, i);
+	i = i >= size ? size - 1 : i;
+	while (j <= i)
 	{
-		printf("Value found between indexes [%d] and [%d]\n",
-		       left - step, left);
-		printf("Value checked array[%d] = [%d]\n",
-		       left - step, array[left - step]);
-	}
-	else
-		printf("Value found between indexes [%d] and [%d]\n",
-		       left, right);
-	if (left >= size || array[left] > value)
-	{
-		return (-1);
-	}
-	right = min(size - 1, right);
-	for (i = left; i <= right && array[i] <= value; i++)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+		j++;
 	}
 	return (-1);
-}
-
-/**
- * min - compare function
- * @a: first parameter to compare
- * @b: second parameter to compare
- *
- * Description: find smallest of the two values
- * Return: smaller value
- */
-int min(int a, int b)
-{
-	return (a < b ? a : b);
 }
