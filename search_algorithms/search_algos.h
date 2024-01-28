5a948e0b9979ce2cd6abc210ba3a4d79adcd4084
Hamid <hemidbeyli2020@gmail.com>
2024-01-28 01:47:34 -0800

    Added binary

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -12,6 +12,7 @@ int jump_search(int *array, size_t size, int value);
 int min(int a, int b);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 
 #endif
